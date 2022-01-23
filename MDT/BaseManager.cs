0fc9d6d29e276237ac343cfc3a0226e107d5b84d
yui <yui@1xp.me>
2022-01-23 08:57:16 -0800

    首次上传

---
+++ MDT/BaseManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public abstract class BaseManager<T> where T : new()
+{
+    private static readonly Lazy<T> lazy = new Lazy<T>();
+    public static T Instance
+    {
+        get
+        {
+            return lazy.Value;
+        }
+    }
+}
+
---
+++ MDT/NativeMethod.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MDT
+{
+    internal class NativeMethod
+    {
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+    }
+}
